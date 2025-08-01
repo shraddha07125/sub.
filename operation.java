0c46548c946cf42d5a4a1ab816a3cb1c690e6689
Sanket Pawar <spawarsanket@gmail.com>
2025-08-01 23:35:45 +0530

    commit

---
+++ operation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+    public class operation
+    {
+        public static void main(String[] args)
+        {
+            int a=52;
+            int b=51;
+            int sub;
+            sub=a-b;
+            System.out.println("subtration of numbers is:" +a+ +b+ +sub);
+        }
+    }
